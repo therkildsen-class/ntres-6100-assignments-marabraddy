203c6202218f582fe8f41e19961f9ea5bf4c4466
marabraddy <marabraddy12@gmail.com>
2025-09-25 10:32:11 -0400

    commit, new PAT

--- course_notes/wrangling1.R
+++ course_notes/wrangling1.R
@@ -197,27 +197,30 @@ gg_base +
     subtitle = "Global sums"
   )
 
+
+# Plot by country-------------------
 coronavirus |> 
   filter(type == "confirmed") |> 
-  group_by(country, date) |> 
+  group_by(date, country) |> 
   summarize(total = sum(cases)) |> 
-  gglot(mapping = aes(x = date, y = total, color = country)) +
-  geom_line()
-  
-coronavirus |> 
-  filter(type == "confirmed") |> 
-  group_by(country) |> 
-  summarize(total = sum(cases)) |> 
-  arrange (-total) |> 
-  head(5) |> 
+  ggplot() +
+    geom_line(mapping = aes(x = date, y = total, color = country))
+
+top5_countries <- coronavirus |>
+  filter(type == "confirmed") |>
+  group_by(country) |>
+  summarize(total = sum(cases)) |>
+  arrange(-total) |>
+  head(5) |>
   pull(country)
-  
 
-
-
-
-
-
+coronavirus |> 
+  filter(type == "confirmed", country %in% top5_countries, cases >= 0) |> 
+  group_by(date, country) |> 
+  summarize(total = sum(cases)) |> 
+  ggplot() +
+  geom_line(mapping = aes(x = date, y = total, color = country)) +
+  facet_wrap(~ country)
 
 
 
