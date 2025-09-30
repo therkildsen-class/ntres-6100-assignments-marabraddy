c0f207089247d3df13d6493f4c578a13ced647bf
marabraddy <marabraddy12@gmail.com>
2025-09-30 11:23:43 -0400

    commit

---
+++ course_notes/Data_format.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(readxl) # install.packages("readxl")
+library(googlesheets4) # install.packages("googlesheets4")
+library(janitor) # install.packages("janitor")
+
+lotr <- read_csv("https://raw.githubusercontent.com/jennybc/lotr-tidy/master/data/lotr_tidy.csv")
+View(lotr)
+
+write_csv(lotr, file = "data/lotr.csv")
+
+lotr <- read_csv("data/lotr.csv")
+getwd()
+lotr <- read_csv("/Users/meb/Desktop/GitHub/ntres-6100-assignments-marabraddy/data/lotr.csv")
+
+lotr <- read_csv("data/lotr.csv", skip = 1, comment = "#")
+
+lotr
+
+
+# Reading from Excel
+
+lotr_excel <- read_xlsx("data/data_lesson11.xlsx", sheet = "FOTR")
+
+gs4_deauth()
+lotr_google <- read_sheet("https://docs.google.com/spreadsheets/d/1X98JobRtA3JGBFacs_JSjiX-4DPQ0vZYtNl_ozqF6IE/edit#gid=754443596", sheet = "FOTR")
+
+
+msa <- read_tsv("https://raw.githubusercontent.com/nt246/NTRES-6100-data-science/main/datasets/janitor_mymsa_subset.txt")
+View(msa)
+
+colnames(msa)
+msa_clean <- clean_names(msa)
+
+cbind(colnames(msa), colnames(msa_clean))
+
+
+# Parse functions
+
+parse_number("$100")
+
+
+
+
