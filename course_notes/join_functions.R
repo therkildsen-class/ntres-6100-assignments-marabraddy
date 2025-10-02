70b66d5fb447de60645f4dbff6b28766584f09c0
marabraddy <marabraddy12@gmail.com>
2025-10-02 11:23:59 -0400

    join functions notes

--- course_notes/join_functions.R
+++ course_notes/join_functions.R
@@ -28,20 +28,58 @@ bind_rows(fship_no_female, ttow, rking)
 flights
 View(flights)
 
-airlines
+airlines # looking for keys
 airports
 
 planes |> 
-  count(tailnum) # how many values/observations for each vairable (like tail number)? We can see that there is one value/observation for each plane tail number.
-
-planes |> 
-  count(year) |> 
-  tail()
+  count(tailnum) # how many values/observations for each variable (like tail number)? We can see that there is one value/observation for each plane tail number.
 
 planes |> 
   count(tailnum) |> 
   filter(n > 1)
 
+planes |> 
+  count(year) |> 
+  tail()
+
+View(weather)
+weather |>
+  count(time_hour, origin)
+  filter(n > 1)
+
+planes |> 
+  filter(is.na(tailnum)) # a way to check that every single row has a tailnum value for the tailnum variable/column
+
+flights2 <- flights |> 
+  select(year:day, hour, origin, dest, tailnum, carrier)
+flights2
+
+# combining the carrier column with the airlines dataset? how to make them match??
+flights2 |> 
+  left_join(airlines)
+
+flights2 |> 
+  left_join(weather)
+
+flights2 |> 
+  left_join(planes, join_by(tailnum))
+# The year variable in 'plane' indicates the year the place was made. The year variable in "flights2" is the year the plane took off. This function helps us join the two sets without that confusion. The set codes year.x and year.y to distinguish the two columns.
+
+# to rename the year.x and year.y columns...
+flights2 |> 
+  left_join(planes, join_by(tailnum), suffix = c("_flight", "_planes"))
+
+
+flights2 |> 
+  left_join(airports, join_by(origin == faa))
+
+
+airports2 <- airports |> 
+  select(faa, name, lat, lon)
+
+flights2 |> 
+  left_join(airports2, join_by(origin == faa)) |> 
+  left_join(airports2, join_by(dest == faa), suffix = c("_origin", "_dest"))
 
 
 
